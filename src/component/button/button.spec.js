93cd1b6198bbd6189b8d097669b9421a8ac2d856
Khoa Do <khoado.dk@gmail.com>
2020-01-09 10:34:13 -0500

    Added event button testing

--- src/component/button/button.spec.js
+++ src/component/button/button.spec.js
@@ -19,13 +19,23 @@ describe('Button component', () => {
   //check render
   describe('Render', () => {
     let wrapper;
+    let mockFunc;
     beforeEach(() => {
-      const props = { buttonText: 'Test text', emitEvent: () => {} };
+      mockFunc = jest.fn();
+      const props = { buttonText: 'Test text', emitEvent: mockFunc };
       wrapper = shallow(<Button {...props} />);
     });
+
     it('Should render a button', () => {
       const button = findByTestAtrr(wrapper, 'buttonComponent');
       expect(button.length).toBe(1);
+    });
+
+    it('Should emit a callback on click event', () => {
+      const button = findByTestAtrr(wrapper, 'buttonComponent');
+      button.simulate('click');
+      const callback = mockFunc.mock.calls.length;
+      expect(callback).toBe(1);
     });
   });
 });
